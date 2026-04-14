23f28bf87f5985e825f1495799be23e8e453843e
ShravasteeRaut <neelampankajraut@gmail.com>
2026-04-14 21:33:14 +0530

    Add files via upload

---
+++ Variable.py
@@ -0,0 +1,16 @@
+x = 5
+y = "John"
+print(x)
+print(y)
+codingal = "Codingal"
+print(codingal)
+name = input("enter your name: ")
+print("\nHello", name, "\nWelcome to Codingal")
+print("Welcome " + name)
+
+a = 10
+b = 20
+print(f"Sum is {a+b}")
+pi = 3.141592
+print(f"{pi} : .2f")
+
