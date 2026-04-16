139e259aef313cdffabb2b291dcc0c5b7888f34d
ShravasteeRaut <neelampankajraut@gmail.com>
2026-04-16 22:30:23 +0530

    Add files via upload

---
+++ Congratutaions.py
@@ -0,0 +1,9 @@
+text = input("Enter your text: ")
+print(text.lower())
+print(text.upper())
+text2 = ["Apple", "Mango", "Banana"]
+text3 = ("#".join(text2))
+print(text3)
+text4 = "TODAY IS A GOOD DAY"
+print(text4.split())
+
