a31168d168abbebfb2be1c19650a85981a680df5
ShravasteeRaut <neelampankajraut@gmail.com>
2026-04-23 21:33:32 +0530

    Add files via upload

---
+++ Greater or Smaller.py
@@ -0,0 +1,9 @@
+i = int(input("Enter any number:"))
+if i<15:
+    print("My value is less than 15")
+    print("I'm smaller than 15")
+    
+else:
+    print("My value is greater than 15")
+
+        
