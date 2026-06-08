118623c296c3008f1953829d4465b7dc0ef79563
ShravasteeRaut <neelampankajraut@gmail.com>
2026-06-08 22:31:51 +0530

    Add files via upload

---
+++ Square.py
@@ -0,0 +1,19 @@
+import turtle
+
+pen = turtle.Turtle()
+
+print("Running square program")
+
+pen.forward(100)
+pen.right(90)
+
+pen.forward(100)
+pen.right(90)
+
+pen.forward(100)
+pen.right(90)
+
+pen.forward(100)
+
+
+turtle.done()
