0a2527b705315d6bca6b3089bb0c54cdfa8b1681
ShravasteeRaut <neelampankajraut@gmail.com>
2026-06-11 22:08:21 +0530

    Add files via upload

---
+++ Circle.py
@@ -0,0 +1,7 @@
+def circle_circumference(P):
+    return 2 * 3.14 * P
+
+print ("Please select the opperation.")
+
+num_1 = int(input("Please enter your number: "))
+print (circle_circumference(num_1))
