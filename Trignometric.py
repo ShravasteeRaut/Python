0e41ca291fa1144a0ff28cff0a62bee5938af61d
ShravasteeRaut <neelampankajraut@gmail.com>
2026-06-25 22:31:24 +0530

    Add files via upload

---
+++ Trignometric.py
@@ -0,0 +1,10 @@
+import math
+
+def trig_values():
+    n = int(input("Enter a value: "))
+
+    print("Sin =", math.sin(n))
+    print("Cos =", math.cos(n))
+    print("Tan =", math.tan(n))
+
+trig_values()
