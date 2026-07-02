3c659feee297b8e7afcda534750762985998e3eb
ShravasteeRaut <neelampankajraut@gmail.com>
2026-07-02 21:52:37 +0530

    Add files via upload

---
+++ Word Matching.py
@@ -0,0 +1,12 @@
+def match_words(words):
+    ctr = 0
+    lst = []
+    for word in words:
+        if len(word) > 1 and words [0] == word[-1]:
+           ctr += 1
+           lst.append(word)
+    print("List of words with first and last character smae\n", lst) 
+    return ctr  
+  
+count = match_words(['abc', 'cfc', 'xyz', 'aba', '1221'])
+print("Number of words having first and lastcharacter same:", count)
